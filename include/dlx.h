b51fe3c476c79c543222e2820249f628fd5b520c
koromaki88 <officialngannhien@gmail.com>
2025-06-21 01:09:49 +0700

    node linking setup, template & consts

--- include/dlx.h
+++ include/dlx.h
@@ -1,8 +1,14 @@
 #ifndef DLX_H
 #define DLX_H
 
+template<int SubGridSize>
 class DLX {
 public:
+    static const int N = SubGridSize * SubGridSize;
+    static const int A = N * N;
+    static const int CONSTRAINT_NUM = 4;
+    static const int COL_NUM = A * CONSTRAINT_NUM;
+
 private:
     struct ColumnNode;
     struct DataNode {
@@ -12,22 +18,36 @@ private:
         DataNode() : L(this), R(this), U(this), D(this), C(nullptr) {}
         DataNode(ColumnNode *c) : L(this), R(this), U(this), D(this), C(c) {}
 
-        void linkDown(DataNode *node);
-        void linkRight(DataNode *node);        
-        void unlinkLR();
-        void relinkLR();
-        void unlinkUD();
-        void relinkUD();
+        void linkDown(DataNode *node) { 
+            node->D = D;
+            node->D->U = node;
+            node->U = this;
+            D = node;
+        };
+        void linkRight(DataNode *node) {
+            node->R = R;
+            node->R->L = node;
+            node->L = this;
+            R = node;
+        };        
+        void unlinkLR() { L->R = R; R->L = L; };
+        void relinkLR() { L->R = this; R->L = this; };
+        void unlinkUD() { U->D = D; D->U = U; };
+        void relinkUD() { U->D = this; D->U = this; };
     };
 
     struct ColumnNode : public DataNode {
         int size;
         int name;
 
-        ColumnNode(int n) : DataNode(), size(0), name(n) { C = this; }
+        ColumnNode(int n = -1) : DataNode(), size(0), name(n) { this->C = this; }
 
-        void cover();
-        void uncover();
+        void cover() {
+            this->unlinkLR();
+        };
+        void uncover() {
+
+        };
     };
 };
 
